2caa6d0462530111b10d0485c1adf5244c4879db
Simone Crepaldi <sccrepaldi@gmail.com>
2023-06-09 13:24:22 -0300

    Exercícios do site Python Brasil.

--- PythonBrasil/EstruturaSequencial/Exercício13.py
+++ PythonBrasil/EstruturaSequencial/Exercício13.py
@@ -15,3 +15,26 @@ elif gênero == 'H':
     print(f"O peso ideal para um homem com {altura} m de altura é {peso_ideal:.2f}kg")
 else:
     print("Informação inválida! Não é possível calcular o peso ideal da pessoa.")
+
+
+# Considerando a opção "outros" para o gênero
+# Para homens: (72.7*h) - 58
+# Para mulheres: (62.1*h) - 44.7
+# Para outros: (65.1*h) - 48.2
+
+altura = float(input('Digite seu altura: '))
+genero = input('Digite H para homem, M para mulher ou outra letra para outros: ')
+if genero == 'h' or genero == 'H':
+    coef1 = 72.7
+    coef2 = 58
+    desc = 'um homem'
+elif genero == 'm' or genero == 'M':
+    coef1 = 62.1
+    coef2 = 44.7
+    desc = 'uma mulher'
+else:
+    coef1 = 65.1
+    coef2 = 48.2
+    desc = 'alguém'
+peso_ideal = (coef1 * altura) - coef2
+print(f"O peso ideal para {desc} de altura {altura:.2f} m é de {peso_ideal:.2f} Kg")
