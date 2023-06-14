9c4bfaec12bdf12152c785988c4599b96853c8af
Simone Crepaldi <sccrepaldi@gmail.com>
2023-06-14 18:17:57 -0300

    exercícios sobre conjuntos

--- Livro_Python/Capítulo_06/Exercício6.19.py
+++ Livro_Python/Capítulo_06/Exercício6.19.py
@@ -1,18 +1,24 @@
 # Escreva um programa que compare duas listas. Utilizando operações com conjuntos, imprima:
-# os valores comuns às duas listas
-# os valores que só existem na primeira
-# os valores que existem apenas na segunda
+# os valores comuns às duas listas (intersecção &)
+# os valores que só existem na primeira (diferença -)
+# os valores que existem apenas na segunda (diferença -)
 # uma lista com os elementos não repetidos das duas listas
 # a primeira lista sem os elementos repetidos na segunda
 
 ListaA = [0, 1, 2, 3, 4, 5]
 ListaB = [1, 3, 5, 7, 9]
+
 a = set(ListaA)
 b = set(ListaB)
-ListaC = (a-b)|(b-a)
+c = a & b
+d = (a|b) - c
+e = a - c
 
-print(f"Valores comuns às duas listas: ")
+print(f"Lista A: {ListaA}")
+print(f"Lista B: {ListaB}")
+print()
+print(f"Valores comuns às duas listas: {c}")
 print(f"Valores que só existem na primeira lista: {a-b}")
 print(f"Valores que existem apenas na segunda: {b-a}")
-print(f"Lista com os elementos que não se repetem: {ListaC}")
-print(f"ListaA sem os elementos repetidos na ListaB: ")
+print(f"Lista com os elementos que não se repetem: {d}")
+print(f"Lista A sem os elementos repetidos na Lista B: {e}")
--- Livro_Python/Capítulo_06/Exercício6.20.py
+++ Livro_Python/Capítulo_06/Exercício6.20.py
@@ -3,3 +3,17 @@
 # os elementos que não mudaram
 # os novos elementos
 # os elementos que foram removidos
+
+Lista_inicial = [0, 3, 8, 15, 18, 20]
+Lista_alterada = [1, 2, 3, 6, 15, 18]
+a = set(Lista_inicial)
+b = set(Lista_alterada)
+c = a & b
+d = b - a
+e = a - b
+
+print(a)
+print(b)
+print(f"Os elementos que não foram alterados da lista: {c}")
+print(f"Os novos elementos: {d}")
+print(f"Os elementos que foram removidos da lista: {e}")
