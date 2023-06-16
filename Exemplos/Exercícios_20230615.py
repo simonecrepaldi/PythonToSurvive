d159030fd80594c5aabd29107151ba906f2ce0a4
Simone Crepaldi <77973522+simonecrepaldi@users.noreply.github.com>
2023-06-16 09:27:20 -0300

    Update Exercícios_20230615.py

--- Exemplos/Exercícios_20230615.py
+++ Exemplos/Exercícios_20230615.py
@@ -2,44 +2,43 @@
 
 # Exercício 1:
 # Crie uma tupla chamada "meses" com os nomes dos meses do ano. Em seguida, imprima o terceiro mês da tupla.
-# meses = ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
-#          "Julho", "Agosto", "Setembro", "Novembro", "Dezembro")
-# print(meses[2])
+meses = ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Novembro", "Dezembro")
+print(meses[2])
 
 
 # Exercício 2
 # Crie uma tupla chamada "coordenadas" com as coordenadas x e y de um ponto.
 # Solicite ao usuário que digite os valores das coordenadas e armazene-os na tupla.
 # Em seguida, imprima as coordenadas do ponto.
-# x = float(input("Digite a coordenada x do ponto: "))
-# y = float(input("Digite a coordenada y do ponto: "))
-# coordenadas = (x, y)
-# print(f"Coordenadas do ponto: x = {coordenadas[0]} e y = {coordenadas[1]}")
-# print("Coordenadas:", coordenadas)
+x = float(input("Digite a coordenada x do ponto: "))
+y = float(input("Digite a coordenada y do ponto: "))
+coordenadas = (x, y)
+print(f"Coordenadas do ponto: x = {coordenadas[0]} e y = {coordenadas[1]}")
+print("Coordenadas:", coordenadas)
 
 
 # Exercício
 # Crie uma tupla chamada "numeros" com alguns números inteiros.
 # Utilizando um loop for, calcule e imprima a soma de todos os números da tupla.
-# numeros = (1, 2, 4, 6, 8)
-# soma = 0
-# for x in numeros:
-#     soma += x
-# print(f"Soma = {soma}")
+numeros = (1, 2, 4, 6, 8)
+soma = 0
+for x in numeros:
+  soma += x
+print(f"Soma = {soma}")
 
 
 # Exercício 4
 # Crie uma tupla chamada "ponto" com as coordenadas x, y e z de um ponto no espaço tridimensional.
 # Imprima cada coordenada em uma linha separada.
-# ponto = (1, 2, 3)
-# print("Coordenadas do ponto:")
-# print(f"x = ", ponto[0])
-# print(f"y = ", ponto[1])
-# print(f"z = ", ponto[2])
+ponto = (1, 2, 3)
+print("Coordenadas do ponto:")
+print(f"x = ", ponto[0])
+print(f"y = ", ponto[1])
+print(f"z = ", ponto[2])
 
 # Outra forma de resolver usando o for
-# for item in ponto:
-#     print(item)
+for item in ponto:
+  print(item)
 
 
 # Exercício 5
@@ -47,10 +46,10 @@
 # Solicite ao usuário que digite um nome e verifique se o nome está presente na tupla.
 # Se estiver, imprima uma mensagem informando que o nome foi encontrado;
 # caso contrário, exiba uma mensagem informando que o nome não foi encontrado.
-# nomes = ("Maria", "João", "Ana", "Pedro")
-# nome = input("Digite um nome: ").title()
-#
-# if nome in nomes:
-#     print(f"O nome {nome} foi encontrado na tupla.")
-# else:
-#     print(f"O nome {nome} não foi encontrado na tupla.")
+nomes = ("Maria", "João", "Ana", "Pedro")
+nome = input("Digite um nome: ").title()
+
+if nome in nomes:
+  print(f"O nome {nome} foi encontrado na tupla.")
+else:
+  print(f"O nome {nome} não foi encontrado na tupla.")
