cf4636c5d888453d5886c92657dc1aeb4a35e352
Simone Crepaldi <77973522+simonecrepaldi@users.noreply.github.com>
2023-06-28 09:54:45 -0300

    Update Exercícios_2023.06.27.py

--- Exemplos/Exercícios_2023.06.27.py
+++ Exemplos/Exercícios_2023.06.27.py
@@ -6,8 +6,7 @@ print(f"Área do círculo: {area:.2f}")
 
 
 # Exercício 2
-# Faça um programa que pergunte o preço de três produtos e
-# informe qual produto você deve comprar, sabendo que a decisão é sempre pelo mais barato.
+# Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar, sabendo que a decisão é sempre pelo mais barato.
 a = float(input(f"Informe o valor do produto 1: "))
 b = float(input(f"Informe o valor do produto 2: "))
 c = float(input(f"Informe o valor do produto 3: "))
@@ -30,8 +29,7 @@ print(f"Você deve comprar o produto {index} que custa R${precos[index-1]:.2f}")
 
 
 # Exercício 3
-# Faça um Programa que peça um número correspondente a um determinado ano
-# e em seguida informe se este ano é ou não bissexto.
+# Faça um Programa que peça um número correspondente a um determinado ano e em seguida informe se este ano é ou não bissexto.
 ano = int(input("Informe o ano: "))
 if ((ano % 4 == 0) and (ano % 100 != 0)) or (ano % 400 == 0) :
     print(f"O ano {ano} é bissexto.")
@@ -40,8 +38,7 @@ else:
 
 
 # Exercício 4:
-# Faça um Programa para leitura de três notas parciais de um aluno.
-# O programa deve calcular a média alcançada por aluno e presentar:
+# Faça um Programa para leitura de três notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e presentar:
 # A mensagem "Aprovado", se a média for maior ou igual a 7, com a respectiva média alcançada;
 # A mensagem "Reprovado", se a média for menor do que 7, com a respectiva média alcançada;
 # A mensagem "Aprovado com Distinção", se a média for igual a 10.
@@ -61,10 +58,8 @@ else:
 
 
 # Exercício 5
-# Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3%
-# e que a população de B seja 200000 habitantes com uma taxa de crescimento de 1.5%.
-# Faça um programa que calcule e escreva o número de anos necessários para que a população do país A ultrapasse
-# ou iguale a população do país B, mantidas as taxas de crescimento.
+# Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3% e que a população de B seja 200000 habitantes com uma taxa de crescimento de 1.5%.
+# Faça um programa que calcule e escreva o número de anos necessários para que a população do país A ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento.
 A = 80000
 B = 200000
 x = 0
