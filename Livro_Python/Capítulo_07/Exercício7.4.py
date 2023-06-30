1858b42f779d7cff9c523f17a1f1afd631a1315f
simonecrepaldi <sccrepaldi@gmail.com>
2023-06-29 21:08:25 -0300

    Update Exercício7.4.py

--- Livro_Python/Capítulo_07/Exercício7.4.py
+++ Livro_Python/Capítulo_07/Exercício7.4.py
@@ -6,3 +6,11 @@ for letra in string:
     else:
         print(f"{letra}: {string.count(letra)}x")
         a += letra
+
+# Outra forma de resolver usando "not in"
+string = "TTAAC"
+a = ""
+for letra in string:
+    if letra not in a:
+        print(f"{letra}: {string.count(letra)}x")
+        a += letra
